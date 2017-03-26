e6e0355f83c6ed044e2b1bfd88dbcd4d8977b5e3
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2017-03-25 23:30:01 -0500

    Add fish completion files

---
+++ fish_completion/sv-disable.fish
@@ -0,0 +1,2 @@
+complete -c sv-disable -p /usr/bin/sv-disable -a "(/bin/ls /var/service/)" -f
+complete -c sv-disable -p /usr/bin/sv-disable -l help -l sv-dir -l runsvdir -s h -s A -s B
---
+++ fish_completion/sv-enable.fish
@@ -0,0 +1,2 @@
+complete -p /usr/bin/sv-enable -a "(/bin/ls /etc/sv/)" -f
+complete -c sv-enable -p /usr/bin/sv-enable -l help -l sv-dir -l runsvdir -s h -s A -s B
